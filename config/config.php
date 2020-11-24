2b8aab40739c0a3ae271bc898de7ea54e4240c5d
jayamirudin <50808338+jayamirudin@users.noreply.github.com>
2020-11-24 10:59:55 +0700

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+$datasa["ipaddress"]	= "10.10.0.1";                     //Ubah ip router anda            wajib
+$datasa["user"]		= "admin";                 //ubah username mirktoik       wajib
+$datasa["password"]	= "bijilumeledak";                //paswword router      wajib
+$datasa["token"]	= "1243847899:AAEynqJugRlp_ArsHpMpEdNSxSy7kRv5ed8";                //token Bot    wajib
+$datasa["usernamebot"]	= "1243847899";           //usernamebot wajib
+$datasa["id"]		= "";   //paswwordbot comingsoon
